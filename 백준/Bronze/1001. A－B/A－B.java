d58e6ab0fd5152ab62dadd35015c2c9e33740c44
손은규 <107801253+kkwabaegi@users.noreply.github.com>
2024-09-03 11:51:18 +0900

    [Bronze V] Title: A-B, Time: 212 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		System.out.println(a-b);
+		
+		
+		
+	}
+
+}
