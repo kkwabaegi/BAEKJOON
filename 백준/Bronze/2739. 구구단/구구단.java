7558b8173e1a7a2353e7ddb81e6a268fb3d584f0
손은규 <107801253+kkwabaegi@users.noreply.github.com>
2024-09-03 12:02:17 +0900

    [Bronze V] Title: 구구단, Time: 236 ms, Memory: 18516 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		for(int i=1;i<10;i++) {
+			System.out.println(a+" * "+i+" = "+a*i);
+		}
+
+	}
+
+}
