c8a4aa6cc2333fb99305f35800035c9233552c7c
손은규 <107801253+kkwabaegi@users.noreply.github.com>
2024-09-03 12:15:00 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        char N = s.next().charAt(0);
+        System.out.println((int)N);
+        
+        s.close();
+	}
+}
