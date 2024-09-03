5021ef549bfbb5a0cfc499532c1c055586461e5c
손은규 <107801253+kkwabaegi@users.noreply.github.com>
2024-09-03 12:05:01 +0900

    [Bronze V] Title: 크냐?, Time: 232 ms, Memory: 17896 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=0,b=0;
+		while(true){
+			a=s.nextInt();
+			b=s.nextInt();
+			if(a==0&&b==0) {
+				break;
+			}
+			if(a>b) {
+				System.out.println("Yes");
+			}
+			else {
+				System.out.println("No");
+			}
+		}
+		
+		
+		
+	}
+
+}
