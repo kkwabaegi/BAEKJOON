ed32e0e5932f69bd7ed99db6cc87b6d73bbe76a0
손은규 <107801253+kkwabaegi@users.noreply.github.com>
2024-09-03 12:07:24 +0900

    [Bronze V] Title: 합, Time: 208 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int h=s.nextInt();
+		int hap=0;
+		for(int i=1;i<=h;i++) {
+			hap+=i;
+		}
+		System.out.println(hap);
+
+	}
+
+}
