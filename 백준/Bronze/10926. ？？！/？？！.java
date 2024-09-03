5b83c882aa441a5d496a3e54bd8562dfa6854f86
손은규 <107801253+kkwabaegi@users.noreply.github.com>
2024-09-03 12:13:01 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		String a=s.next();
+		System.out.println(a+"??!");
+	}
+
+}
