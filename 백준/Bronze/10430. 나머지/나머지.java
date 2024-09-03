e8ecb926259ebec06fd9dc125e23e83b28d12ac5
손은규 <107801253+kkwabaegi@users.noreply.github.com>
2024-09-03 12:10:04 +0900

    [Bronze V] Title: 나머지, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int A=s.nextInt();
+		int B=s.nextInt();
+		int C=s.nextInt();
+		System.out.println((A+B)%C);
+		System.out.println(((A%C) + (B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C) * (B%C))%C);
+	}
+
+}
