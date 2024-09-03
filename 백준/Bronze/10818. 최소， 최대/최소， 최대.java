198333e84c45d0c92a09fcc3652685c4a38dd2e2
손은규 <107801253+kkwabaegi@users.noreply.github.com>
2024-09-03 12:12:36 +0900

    [Bronze III] Title: 최소, 최대, Time: 2408 ms, Memory: 321940 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+    	Scanner s=new Scanner(System.in);
+    	int n=s.nextInt();
+    	int a[]=new int[n];
+    	for(int i=0;i<n;i++) {
+    		a[i]=s.nextInt();
+    	}
+    	Arrays.sort(a);
+    	System.out.println(a[0]+" "+a[n-1]);
+    	
+    	
+    }
+
+}
