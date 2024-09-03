a81edc1c9f128ac26b8e0c3d9e3dd478ce4e77dd
손은규 <107801253+kkwabaegi@users.noreply.github.com>
2024-09-03 12:13:07 +0900

    [Bronze V] Title: A+B - 3, Time: 232 ms, Memory: 17924 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int h=s.nextInt();
+		int a=0;
+		int b=0;
+		for(int i=0;i<h;i++) {
+			a=s.nextInt();
+			b=s.nextInt();
+			System.out.println(a+b);
+		}
+
+	}
+
+}
