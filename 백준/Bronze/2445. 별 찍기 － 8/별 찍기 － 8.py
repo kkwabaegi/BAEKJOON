d8aac7aff09f4ca0da000ce10f88c4c25b46c4c6
손은규 <107801253+kkwabaegi@users.noreply.github.com>
2024-09-19 12:23:04 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(n):
+    print('*'*(i+1),end='')
+    print(' '*(n-i-1)*2,end='')
+    print('*'*(i+1))
+for i in range(n-1):
+    print('*'*(n-1-i),end='')
+    print(' '*(i+1)*2,end='')
+    print('*'*(n-1-i))
